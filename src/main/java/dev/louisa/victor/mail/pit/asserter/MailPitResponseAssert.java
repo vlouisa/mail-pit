45e03dc1b9f1b19a7dfba2893902ca1fba8ea0ed
Victor Louisa <victor.louisa@gmail.com>
2025-09-14 15:11:53 +0200

    add await functionality when fetching/asserting messages (#2)

--- src/main/java/dev/louisa/victor/mail/pit/asserter/MailPitResponseAssert.java
+++ src/main/java/dev/louisa/victor/mail/pit/asserter/MailPitResponseAssert.java
@@ -1,19 +1,55 @@
 package dev.louisa.victor.mail.pit.asserter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.louisa.victor.mail.pit.api.MailPitApi;
 import dev.louisa.victor.mail.pit.model.MailPitResponse;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.ObjectAssert;
 
-public class MailPitResponseAssert extends AbstractAssert<MailPitResponseAssert, MailPitResponse> {
+import java.time.Duration;
 
+import static org.awaitility.Awaitility.await;
+
+public class MailPitResponseAssert extends AbstractAssert<MailPitResponseAssert, MailPitResponse> {
     protected MailPitResponseAssert(MailPitResponse actual) {
         super(actual, MailPitResponseAssert.class);
     }
 
-    public static MailPitResponseAssert assertThatMailPitResponse(MailPitResponse actual) {
-        return new MailPitResponseAssert(actual);
+    public static MailPitResponseInput mailPitMessages() {
+        return new MailPitResponseInput();
     }
+    
+    public static class MailPitResponseInput {
+        private String baseUri;
+        private int expectedCount = 0;
 
+        public MailPitResponseInput fromBaseUri(String baseUri) {
+            this.baseUri = baseUri;
+            return this;
+        }
+        
+        public MailPitResponseInput awaitMessages(int expectedCount) {
+            this.expectedCount = expectedCount;
+            return this;
+        }
+
+        public MailPitResponseAssert assertThat() throws JsonProcessingException {
+            if(expectedCount <= 0) {
+                return new MailPitResponseAssert(MailPitApi.fetchMessages(baseUri));
+            }
+            
+            await().atMost(Duration.ofSeconds(10))
+                    .pollInterval(Duration.ofMillis(500))
+                    .until(() -> {
+                        MailPitResponse response = MailPitApi.fetchMessages(baseUri);
+                        return response.messages().size() >= expectedCount;
+                    });
+
+            return new MailPitResponseAssert(MailPitApi.fetchMessages(baseUri));
+        }
+    }
+    
+    // Existing fluent methods
     public MailPitMessageAssert message(int messageNumber) {
         isNotNull();
 
@@ -23,11 +59,14 @@ public class MailPitResponseAssert extends AbstractAssert<MailPitResponseAssert,
 
         if (actual.messages().size() < messageNumber) {
             failWithMessage(
-                    "Total number of message in response is <%s>, requested message <%s> doesn't exist"
-                    , actual.messages().size()
-                    , messageNumber);
+                    "Total number of messages in response is <%s>, requested message <%s> doesn't exist",
+                    actual.messages().size(),
+                    messageNumber);
         }
-        return MailPitMessageAssert.assertMailPitMessage(actual.messages().get(actual.messages().size() - messageNumber));
+
+        return MailPitMessageAssert.assertMailPitMessage(
+                actual.messages().get(actual.messages().size() - messageNumber)
+        );
     }
 
     public ObjectAssert<Integer> numberOfMessages() {
